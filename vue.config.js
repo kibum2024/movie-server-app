8fcd6dfe9a184e413f6db7b9f784e99adf90f6af
dnflboy <dnflboy@gmail.com>
2024-08-02 13:50:37 +0900

    최초

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/movie-server-app/',
+  outputDir: 'dist' 
 })
